b6584222cde769106e277c00fdcc32938f911814
gost12 <mihail-kozlov@ya.ru>
2025-02-26 22:56:39 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -13,14 +13,14 @@ sed -i 's/UA/RU/' padavan-ng/trunk/user/shared/defaults.h
 sed -i 's/UA/RU/' padavan-ng/trunk/user/shared/defaults.h
 
 # Custom defaults Wi-Fi SSID
-sed -i 's/"Padavan_2.4GHz"/BOARD_PID \"\_\%\s\"/' padavan-ng/trunk/user/shared/defaults.h
-sed -i 's/"Padavan_5GHz"/BOARD_PID \"\_\%\s\_5G"/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/"Padavan_2.4GHz"/Xiaomi_2.4GHz"/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/"Padavan_5GHz"/Xiaomi_5G"/' padavan-ng/trunk/user/shared/defaults.h
 
-sed -i 's/"Padavan_GUEST_2.4GHz"/BOARD_PID \"\_Guest_\%\s\"/' padavan-ng/trunk/user/shared/defaults.h
-sed -i 's/"Padavan_GUEST_5GHz"/BOARD_PID \"\_Guest_\%\s\_5G"/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/"Padavan_GUEST_2.4GHz"/Xiaomi_Guest"/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/"Padavan_GUEST_5GHz"/Xiaomi_Guest_5G"/' padavan-ng/trunk/user/shared/defaults.h
 
 # Default timezone
-sed -i 's/EET-2EETDST/MST-3MDT/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/EET-2EETDST/MSK/' padavan-ng/trunk/user/shared/defaults.h
 
 # NTP servers
 sed -i 's/pool.ntp.org/ru.pool.ntp.org/' padavan-ng/trunk/user/shared/defaults.h
