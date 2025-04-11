bf170ad1bffa52c15f754f573480fab3699b3d91
gost12 <mihail-kozlov@ya.ru>
2025-04-11 22:39:34 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -73,7 +73,7 @@ sed -i 's/EET-2EETDST/UTC-3/' padavan-ng/trunk/user/shared/defaults.h
 
 # NTP servers
 sed -i 's/pool.ntp.org/ru.pool.ntp.org/' padavan-ng/trunk/user/shared/defaults.h
-sed -i 's/time.in.ua/time.nist.gov/' padavan-ng/trunk/user/shared/defaults.h
+sed -i 's/time.in.ua/ntp.ix.ru/' padavan-ng/trunk/user/shared/defaults.h
 
 # Footer edit
 #sed -i 's/^LABEL_COPYRIGHT_TEXT=.*/LABEL_COPYRIGHT_TEXT="© 2011-`date +\\u0027\\u0025\\u0059\\u0027` Padavan \\u005C\\u0026 N56U Project Community"/' padavan-ng/trunk/user/www/Makefile
